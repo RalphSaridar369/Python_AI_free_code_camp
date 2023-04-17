6f2d0593377bafab4603685f6fe22b0c3ea8675c
RalphSaridar55 <ralphsaridar@hotmail.com>
2023-04-17 03:47:06 +0300

    linear regression

--- 2_linear_regression/loading_data_set/loading_data.py
+++ 2_linear_regression/loading_data_set/loading_data.py
@@ -27,3 +27,42 @@ y_eval = dfeval.pop('survived')
 
 print("training data: ",dftrain)
 print("testing data: ",dfeval)
+
+
+
+'''
+The pd.read_csv() method will return to us a new pandas *dataframe*. You can think of a dataframe like a table. In fact, we can actually have a look at the table representation.
+
+We've decided to pop the "survived" column from our dataset and store it in a new variable. This column simply tells us if the person survived our not.
+
+To look at the data we'll use the  .head() method from pandas. This will show us the first 5 items in our dataframe.
+'''
+
+print("Head: ",dftrain.head())
+
+
+'''
+And if we want a more statistical analysis of our data we can use the .describe() method.
+'''
+
+print("Description: ",dftrain.describe())
+
+
+'''
+And since we talked so much about shapes in the previous tutorial let's have a look at that too!
+'''
+
+print("Shape: ",dftrain.shape)
+
+
+
+'''
+Notice that each entry is either a 0 or 1. Can you guess which stands for survival?
+
+And now because visuals are always valuable let's generate a few graphs of the data.
+'''
+
+dftrain.age.hist(bins=20)
+dftrain.sex.value_counts().plot(kind='barh')
+dftrain['class'].value_counts().plot(kind='barh')
+pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survive')
